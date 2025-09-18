134a5408f475222ed24192e3fee0b68f8cc9f9d9
Spacing Kosmos <spacingkosmopan.mapy@gmail.com>
2025-09-18 13:28:58 +0200

    Dodałem treść zadania do zrobienia w App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
 
+/* CEL:
+ * Stworzenie prostej aplikacji, która pozwala użytkownikowi dodawać produkty do koszyka,
+ * usuwać je oraz zaznaczać, które produkty zostały kupione.
+ * Produkty kupione powinny być stylizowane inaczej niż te, które nadal są do kupienia.
+ */
+
 function App() {
   return <></>;
 }
